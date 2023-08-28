58f57891f209ba5bcc154881faff41317fee5282
kmssinc <kmssinc2017@gmail.com>
2023-08-27 21:28:08 -0400

    end of section 3

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,14 +1,15 @@
-using MongoDB.Driver;
-using MongoDB.Entities;
+using System.Net;
+using Polly;
+using Polly.Extensions.Http;
 using SearchService.Data;
-using SearchService.Models;
 using SearchService.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddHttpClient<AuctionServiceHttpclient>();
+builder.Services.AddHttpClient<AuctionServiceHttpclient>()
+.AddPolicyHandler(GetPolicy());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
@@ -16,11 +17,22 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseAuthorization();
-try{
-    await DbInitializer.InitDb(app);
-} catch(Exception ex)
-{
-    Console.WriteLine(ex);
-}
+
 app.MapControllers();
+
+app.Lifetime.ApplicationStarted.Register(async () => {
+    try{
+        await DbInitializer.InitDb(app);
+    } catch(Exception ex)
+    {
+        Console.WriteLine(ex);
+    }
+});
+
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+=> HttpPolicyExtensions
+.HandleTransientHttpError()
+.OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+.WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
